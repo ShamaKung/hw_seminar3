85e029b273866a8941e9341500c964afc964dfa8
ShamaKung <shamaturin@gmail.com>
2022-12-11 22:53:12 +0500

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B (яв-ся степенью для числа А)");
+int lengthB = Convert.ToInt32(Console.ReadLine());
+
+int res = 1;
+
+int [] n = new int [lengthB];
+
+for (int i = 0; i < lengthB; i++)
+{
+    n[i] = A;
+}
+
+for (int i = 0; i < lengthB; i++)
+{
+    res = res * n[i];
+}
+
+Console.WriteLine(res);
