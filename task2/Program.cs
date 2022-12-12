98ce8a40aa6689548dcb15dc5fa0ae28e18001d5
ShamaKung <shamaturin@gmail.com>
2022-12-12 22:07:41 +0500

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int R = N;
+int res = 0;
+
+while(R > 0)
+{
+   int dev = R % 10;
+
+   if(dev == 0 || N % dev == 0 ) 
+   {
+        res += dev;
+   }
+   R = R / 10;
+}
+Console.Write(res);
